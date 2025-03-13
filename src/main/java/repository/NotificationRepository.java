01c94011b75d972f7bb076e98b690e29f28cfee1
ivanzahariev05 <ivanzaxariev2005@abv.bg>
2025-03-13 19:20:07 +0200

    Repositories

---
+++ src/main/java/repository/NotificationRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+}
