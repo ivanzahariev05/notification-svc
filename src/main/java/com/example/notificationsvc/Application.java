89dddd55b1d9f6442769d5d748b6fbd160ddb4bf
ivanzahariev05 <ivanzaxariev2005@abv.bg>
2025-03-19 10:36:59 +0200

    Scheduling enabled

--- src/main/java/com/example/notificationsvc/Application.java
+++ src/main/java/com/example/notificationsvc/Application.java
@@ -2,12 +2,14 @@ package com.example.notificationsvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class NotificationSvcApplication {
+@EnableScheduling
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(NotificationSvcApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
